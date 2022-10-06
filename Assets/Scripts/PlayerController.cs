2a9b3038cdd551f69bae03e9bf157f23d48b7c1f
EloToJa <lukaszbudziak122@gmail.com>
2022-10-06 17:21:49 +0200

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 12f;
     Vector3 velocity;
     CharacterController characterController;
+    public Transform groundCheck;
+    public LayerMask groundMask;
 
     void Start()
     {
@@ -15,10 +17,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask))
+        {
+
+        }
     }
 }
